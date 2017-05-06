5b1909e4e742d308660032cf22057b329ad16bb2
Keith E. Freeman <conrad@code550.info>
2017-05-06 01:42:05 -0400

    Bug fix in curated destination regex
    Missing end-of-string token caused false positive on duplicate matches

--- src/PdfParser.php
+++ src/PdfParser.php
@@ -32,7 +32,7 @@ class PdfParser extends \fpdi_pdf_parser
         $destination = str_replace('-', '#2d', $destination);
 
         foreach ($this->getDestinations()[1][1] as $dest => $objSpec) {
-            if (preg_match("|^/file#3a#2f#2f#2f.+#23$destination|", $dest)) {
+            if (preg_match("|^/file#3a#2f#2f#2f.+#23$destination$|", $dest)) {
                 return $this->resolveObject($objSpec)[1][1][0][1] + 1 + $this->getPageOffset();
             }
         }
